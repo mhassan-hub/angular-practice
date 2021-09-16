09d00068ac458e4951460caaf72bb2ed9d042872
Mohamed Hassan <mhass405@gmail.com>
2021-09-16 13:54:15 -0400

    Set model for recipes and add layout of components

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Checking if this works', 'https://www.recipetineats.com/wp-content/uploads/2021/08/Spaghetti-Puttanesca_64.jpg?resize=650,910'),
+    new Recipe('A Test Recipe', 'Checking if this works', 'https://www.recipetineats.com/wp-content/uploads/2021/08/Spaghetti-Puttanesca_64.jpg?resize=650,910')
+  ];
   constructor() { }
 
   ngOnInit(): void {
